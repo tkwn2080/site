45571dee5a1aaa5d151b22d6d9d390b5203917f7
B <135119655+tkwn2080@users.noreply.github.com>
2024-08-10 21:09:05 +0100

    "safety"

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    devServer: {
+      headers: {
+        'Content-Security-Policy': "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:;"
+      }
+    }
+  };
