a5125d9036c703d337feef181f032715402fa3af
B <135119655+tkwn2080@users.noreply.github.com>
2024-08-11 13:32:38 +0100

    Update SubstrateDesignerPage.js

--- src/pages/SubstrateDesignerPage.js
+++ src/pages/SubstrateDesignerPage.js
@@ -5,7 +5,7 @@ import SubstrateDesigner from '../components/SubstrateDesigner';
 function SubstrateDesignerPage() {
   return (
     <div className="p-4">
-      <Link to="/" className="text-green-500 hover:text-green-700 mb-4 inline-block">
+      <Link to="/" className="text-[#002D00]-500 hover:text-green-700 mb-4 inline-block">
         ←
       </Link>
       <SubstrateDesigner />
