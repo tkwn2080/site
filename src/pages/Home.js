756e9647819420e56f571242c4451c483772b7c7
B <135119655+tkwn2080@users.noreply.github.com>
2024-08-12 12:39:57 +0100

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-white">
-      <h1 className="text-8xl leading-none text-custom-green-700 mb-8" style={{ fontFamily: 'Times New Roman, serif' }}>b's page</h1>
+      <h1 className="text-8xl leading-none text-custom-green-700 mb-8" style={{ fontFamily: 'Times New Roman, serif' }}>b’s page</h1>
       <nav>
         <ul className="space-y-4">
           <li>
