e419239257e367b4a1c597d969713d7116d8174f
B <135119655+tkwn2080@users.noreply.github.com>
2024-08-12 12:32:44 +0100

    routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import SubstrateDesigner from './components/SubstrateDesigner';
+import SubstrateDesignerPage from './pages/SubstrateDesignerPage';
 
 function App() {
   return (
     <Router basename="/">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/substrate-designer" element={<SubstrateDesigner />} />
+        <Route path="/substrate-designer" element={<SubstrateDesignerPage />} />
       </Routes>
     </Router>
   );
